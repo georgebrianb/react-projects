a55b39e810d6a5e37b721fe02ada2fa10b00b9c5
BUSILA George Brian OSE <George.Busila@office.orange.intra>
2021-12-14 16:54:57 +0200

    started working on stripe menu project:

---
+++ 13-stripe-menu/setup/src/Navbar.js
@@ -0,0 +1,34 @@
+import React from "react";
+import logo from "./images/logo.svg";
+import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
+
+const Navbar = () => {
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="stripe" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">Products</button>
+          </li>
+          <li>
+            <button className="link-btn">Developers</button>
+          </li>
+          <li>
+            <button className="link-btn">Company</button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">Sign In</button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
