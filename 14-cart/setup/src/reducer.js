95dd91d6a171e331d4b3754cb766473116ab5a7d
georgebrianb <george_brianb@yahoo.com>
2021-12-17 17:45:41 +0200

    working on refactoring

--- 14-cart/setup/src/reducer.js
+++ 14-cart/setup/src/reducer.js
@@ -1,3 +1,5 @@
+import CartItem from "./CartItem";
+
 const reducer = (state, action) => {
   switch (action.type) {
     case "CLEAR_CART":
@@ -8,31 +10,44 @@ const reducer = (state, action) => {
         ...state,
         cart: state.cart.filter((item) => item.id !== action.payload),
       };
-    case "INCREASE_AMOUNT":
-      let tempCart = state.cart.map((cartItem) => {
-        if (cartItem.id === action.payload) {
-          return { ...cartItem, amount: cartItem.amount + 1 };
-        }
-        return cartItem;
-      });
-      return {
-        ...state,
-        cart: tempCart,
-      };
-
-    case "DECREASE_AMOUNT":
-      let newCart = state.cart
-        .map((cartItem) => {
-          if (cartItem.id === action.payload) {
+    case "TOGGLE_AMOUNT":
+      let temporaryCart = state.cart.map((cartItem) => {
+        if (cartItem.id === action.payload.id) {
+          if (action.payload.type === "inc") {
+            return { ...cartItem, amount: cartItem.amount + 1 };
+          }
+          if (action.payload.type === "dec") {
             return { ...cartItem, amount: cartItem.amount - 1 };
           }
           return cartItem;
-        })
-        .filter((item) => item.amount > 0);
-      return {
-        ...state,
-        cart: newCart,
-      };
+        }
+      });
+      return { ...state, cart: temporaryCart };
+    // case "INCREASE_AMOUNT":
+    //   let tempCart = state.cart.map((cartItem) => {
+    //     if (cartItem.id === action.payload) {
+    //       return { ...cartItem, amount: cartItem.amount + 1 };
+    //     }
+    //     return cartItem;
+    //   });
+    //   return {
+    //     ...state,
+    //     cart: tempCart,
+    //   };
+
+    // case "DECREASE_AMOUNT":
+    //   let newCart = state.cart
+    //     .map((cartItem) => {
+    //       if (cartItem.id === action.payload) {
+    //         return { ...cartItem, amount: cartItem.amount - 1 };
+    //       }
+    //       return cartItem;
+    //     })
+    //     .filter((item) => item.amount > 0);
+    //   return {
+    //     ...state,
+    //     cart: newCart,
+    //   };
     case "GET_TOTAL":
       const { total, amount } = state.cart.reduce(
         (cartTotal, item) => {
@@ -44,6 +59,10 @@ const reducer = (state, action) => {
         { total: 0, amount: 0 }
       );
       return { ...state, total: total.toFixed(2), amount };
+    case "LOADING":
+      return { ...state, loading: true };
+    case "DISPLAY_ITEMS":
+      return { ...state, cart: action.payload, loading: false };
   }
   return state;
 };
