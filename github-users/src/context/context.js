b5ad1d7195d61fad3df3d80478ce99a6a1dd1bae
georgebrianb <george_brianb@yahoo.com>
2022-01-19 11:20:14 +0200

    test pushing

--- github-users/src/context/context.js
+++ github-users/src/context/context.js
@@ -12,8 +12,13 @@ const GithubContext = React.createContext();
 // since the usecontext hook we mostly use the provider
 
 const GithubProvider = ({ children }) => {
+  const [githubUser, setGithubUser] = useState(mockUser);
+  const [repos, setRepos] = useState(mockRepos);
+  const [followers, setFollowers] = useState(mockFollowers);
   return (
-    <GithubContext.Provider value="hello">{children}</GithubContext.Provider>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
+      {children}
+    </GithubContext.Provider>
   );
 };
 
