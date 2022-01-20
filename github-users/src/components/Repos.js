dcb1640734c1d3d896057100c5b91fe24a63e4f4
georgebrianb <george_brianb@yahoo.com>
2022-01-20 11:41:09 +0200

    finished all charts

--- github-users/src/components/Repos.js
+++ github-users/src/components/Repos.js
@@ -1,9 +1,78 @@
-import React from 'react';
-import styled from 'styled-components';
-import { GithubContext } from '../context/context';
-import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from './Charts';
+import React from "react";
+import styled from "styled-components";
+import { GithubContext } from "../context/context";
+import { ExampleChart, Pie3D, Column3D, Bar3D, Doughnut2D } from "./Charts";
 const Repos = () => {
-  return <h2>repos component</h2>;
+  const { repos } = React.useContext(GithubContext);
+
+  let languages = repos.reduce((total, item) => {
+    const { language, stargazers_count: stars } = item;
+
+    if (!language) return total;
+    if (!total[language]) {
+      total[language] = { label: language, value: 1, stars: stars };
+    } else {
+      total[language] = {
+        ...total[language],
+        value: total[language].value + 1,
+        stars: total[language].stars + stars,
+      };
+    }
+    return total;
+  }, {});
+
+  const mostUsed = Object.values(languages)
+    .sort((a, b) => {
+      return b.value - a.value;
+    })
+    .slice(0, 5);
+  console.log(languages);
+
+  // most stars per language
+  let mostPopular = Object.values(languages);
+  mostPopular = mostPopular
+    .map((item) => {
+      const newArray = {
+        label: item.label,
+        value: item.stars,
+      };
+      return newArray;
+    })
+    .sort((a, b) => {
+      return b.stars - a.stars;
+    })
+    .slice(0, 5);
+
+  // stars, forks
+  let { stars, forks } = repos.reduce(
+    (total, item) => {
+      const { name, stargazers_count: stars, forks } = item;
+      total.stars.push({
+        label: name,
+        value: stars,
+      });
+      total.forks.push({
+        label: name,
+        value: forks,
+      });
+      return total;
+    },
+    { stars: [], forks: [] }
+  );
+
+  const mostStars = stars.sort((a, b) => b.value - a.value).splice(0, 5);
+  const mostForks = forks.sort((a, b) => b.value - a.value).splice(0, 5);
+
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        <Pie3D data={mostUsed}></Pie3D>
+        <Column3D data={mostStars}></Column3D>
+        <Doughnut2D data={mostPopular}></Doughnut2D>
+        <Bar3D data={mostForks}></Bar3D>
+      </Wrapper>
+    </section>
+  );
 };
 
 const Wrapper = styled.div`
